ba26f4f6558425d49cb0cf2cd3b7a6258477be81
DeveloperTommy <its.tommy.nguyen@gmail.com>
2015-12-07 18:21:57 -0500

    Changed views folder

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 var express = require('express');
-var path    = require("path");
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname + '/public'));
 
-app.get('/', (req, res) => {
-	res.sendFile(path.join(__dirname+ '/public/views/index.html'));
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('index');
 });
 
-app.listen(app.get('port'), () => {
-	console.log('Express started on http://localhost:' +
-	app.get('port') + '; press Ctrl-C to terminate');
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
