11df088189ad436805850f0444bd1c7b2bfa3fdf
DeveloperTommy <its.tommy.nguyen@gmail.com>
2015-12-07 18:09:09 -0500

    Attempting to debug heroku

--- index.js
+++ index.js
@@ -10,20 +10,6 @@ app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname+ '/public/views/index.html'));
 });
 
-function notFound404(req, res) {
-	res.status(404);
-	res.render('404.html');
-}
-
-function internalServerError500(err, req, res, next) {
-	console.error(err.stack);
-	res.status(500);
-	res.render('500.html');
-}
-
-//app.use(notFound404);
-//app.use(internalServerError500);
-
 app.listen(app.get('port'), () => {
 	console.log('Express started on http://localhost:' +
 	app.get('port') + '; press Ctrl-C to terminate');
