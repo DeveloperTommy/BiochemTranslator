dd0a8be9e71767a89a2201a83101264f7b0695cd
DeveloperTommy <its.tommy.nguyen@gmail.com>
2015-12-07 20:12:28 -0500

    Prevention of accidentaly enter key press on form

--- public/js/home.js
+++ public/js/home.js
@@ -159,3 +159,20 @@ function complementary() {
   document.getElementById("complement").value = "";
   console.log(response);
 }
+
+$('input').on("keypress", function(e) {
+            /* ENTER PRESSED*/
+            if (e.keyCode == 13) {
+                /* FOCUS ELEMENT */
+                var inputs = $(this).parents("form").eq(0).find(":input");
+                var idx = inputs.index(this);
+
+                if (idx == inputs.length - 1) {
+                    inputs[0].select()
+                } else {
+                    inputs[idx + 1].focus(); //  handles submit buttons
+                    inputs[idx + 1].select();
+                }
+                return false;
+            }
+        });
